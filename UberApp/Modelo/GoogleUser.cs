394b77b463e96e6c0ffc6d2b04c5b82647be4203
Steven Galarza <stegalarza@hotmail.com>
2022-06-16 17:03:44 -0500

    last changes

---
+++ UberApp/Modelo/GoogleUser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UberApp.Modelo
+{
+    public class GoogleUser
+    {
+        public string Name { get; set; }
+        public string Apellido { get; set; }
+
+        public string Email { get; set; }
+
+        public Uri Picture { get; set; }
+    }
+
+    public interface IGoogleManager
+    {
+        void Logout();
+        void Login(Action<GoogleUser,string> OnLogiComplete);
+
+    }
+}
